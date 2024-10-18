c5c31d20f9067a914676ddc0ccd0b580ab1105dd
Andreas Arrgård <andreas.arrgard@realalliance.se>
2024-10-18 15:35:13 +0200

    Az timer functions now handles middlewares

--- SolidRpc.OpenApi.AzFunctionsV2/TimerFunction.cs
+++ SolidRpc.OpenApi.AzFunctionsV2/TimerFunction.cs
@@ -23,12 +23,15 @@ namespace SolidRpc.OpenApi.AzFunctions
         /// <param name="timerId"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public static async Task Run(TimerInfo myTimer, ILogger log, IServiceProvider serviceProvider, string timerId, CancellationToken cancellationToken)
+        public static Task Run(TimerInfo myTimer, ILogger log, IServiceProvider serviceProvider, string timerId, CancellationToken cancellationToken)
         {
-            await serviceProvider.GetRequiredService<ISolidRpcApplication>().WaitForStartupTasks();
-            var ts = serviceProvider.GetRequiredService<ITimerStore>();
-            var action = ts.GetTimerAction(timerId);
-            await action.Invoke(serviceProvider, cancellationToken);
+            return FuncExecutor.ExecuteFunction(serviceProvider, log, myTimer, async () =>
+            {
+                await serviceProvider.GetRequiredService<ISolidRpcApplication>().WaitForStartupTasks();
+                var ts = serviceProvider.GetRequiredService<ITimerStore>();
+                var action = ts.GetTimerAction(timerId);
+                await action.Invoke(serviceProvider, cancellationToken);
+            });
         }
     }
 }
